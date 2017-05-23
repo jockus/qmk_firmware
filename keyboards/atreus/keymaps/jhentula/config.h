cbe12f525c6df71189d766f56a5abf5571656a99
Joakim Hentula <jhentula@radiantworlds.com>
2017-05-23 16:44:28 +0100

    Layout updates

--- keyboards/atreus/keymaps/jhentula/config.h
+++ keyboards/atreus/keymaps/jhentula/config.h
@@ -4,13 +4,13 @@
 #include "../../config.h"
 
 #define PREVENT_STUCK_MODIFIERS
-#define TAPPING_TERM 250
+#define TAPPING_TERM 1500
 
+#define MOUSEKEY_ENABLE = yes
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          5
 #define MOUSEKEY_TIME_TO_MAX    60
 #define MOUSEKEY_MAX_SPEED      7
 #define MOUSEKEY_WHEEL_DELAY	0
-#define FORCE_NKRO 1
 
 #endif
--- keyboards/atreus/keymaps/jhentula/keymap.c
+++ keyboards/atreus/keymaps/jhentula/keymap.c
@@ -15,7 +15,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [ALPH] = {
   {KC_Q,	    KC_W,		   KC_E,		  KC_R,		    KC_T,	    KC_TRNS,	    KC_Y,		KC_U,		 KC_I,		 KC_O,		   KC_P},	
-  {KC_A,	    LT(FKEY, KC_S),	   LT(NUMS, KC_D),	  LT(CURS,KC_F),    KC_G,	    KC_TRNS,	    KC_H,		KC_J,		 KC_K,		 KC_L,		   KC_SCLN},	
+  {KC_A,	    LT(FKEY, KC_S),	   KC_D,		  LT(CURS,KC_F),    KC_G,	    KC_TRNS,	    KC_H,		KC_J,		 KC_K,		 KC_L,		   KC_SCLN},	
   {KC_Z,	    KC_X,		   KC_C,		  KC_V,		    KC_B,	    LALT_T(KC_ESC),	    KC_N,		KC_M,		 KC_COMM,	 KC_DOT,	   KC_SLSH},	
   {LT(GUI,KC_ESC),  TG(MOUS),		   KC_LGUI,	      LT(SYMB,KC_TAB),	  SFT_T(KC_BSPC),   RCTL_T(KC_ENT),	    RSFT_T(KC_SPC),	LT(SYMB,KC_QUOT),KC_TAB,	 TG(GAME),	   KC_ENT}
 },
@@ -32,10 +32,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TRNS,	 KC_TRNS,		    KC_TRNS,		    KC_TRNS,	 KC_TRNS,	 KC_TRNS,	 KC_TRNS,		KC_TRNS,	 KC_TRNS,	 KC_TRNS,	 KC_TRNS}
 },
 [SYMB] = {
-  {KC_EXLM,	 KC_AT,			    KC_HASH,		    KC_DLR,	 KC_PERC,	 KC_TRNS,	 KC_CIRC,		KC_AMPR,	 KC_ASTR,	KC_MINS,	 KC_PLUS},	
-  {KC_AT,	 KC_LBRC,		    KC_LCBR,		    KC_LPRN,	 KC_TRNS,	 KC_TRNS,	 KC_EQL,		KC_RPRN,	 KC_RCBR,	KC_RBRC,	 KC_GRV},	
-  {KC_HASH,	 KC_PIPE,		    KC_LCBR,		    KC_RCBR,	 KC_SLSH,	 KC_TRNS,	 KC_UNDS,		KC_RPRN,	 KC_RCBR,	KC_RBRC,	 KC_BSLS},	
-  {KC_TRNS,	 KC_TRNS,		    KC_LBRC,		    KC_RBRC,	 KC_TRNS,	 KC_TRNS,	 KC_TRNS,		KC_TRNS,	 KC_TRNS,	KC_TRNS,	 KC_TRNS}
+  {KC_EXLM,	 KC_AT,			    KC_LBRC,		    KC_RBRC,	 KC_ASTR,	 KC_TRNS,	 KC_CIRC,		KC_7,	 KC_8,	KC_9,	 KC_PLUS},	
+  {KC_HASH,	 KC_DLR,		    KC_LPRN,		    KC_RPRN,	 KC_MINS,	 KC_TRNS,	 KC_EQL,		KC_4,	 KC_5,	KC_6,	 KC_GRV},	
+  {KC_PERC,	 KC_AMPR,		    KC_LCBR,		    KC_RCBR,	 KC_SLSH,	 KC_TRNS,	 KC_UNDS,		KC_1,	 KC_2,	KC_3,	 KC_BSLS},	
+  {KC_TRNS,	 KC_TRNS,		    KC_TRNS,		    KC_TRNS,	 KC_TRNS,	 KC_TRNS,	 KC_0,		KC_TRNS,	 KC_TRNS,	KC_TRNS,	 KC_TRNS}
 },
 [FKEY] = {
   {KC_TRNS,	 KC_TRNS,		    KC_TRNS,		    KC_TRNS,	 KC_TRNS,	 KC_TRNS,	 KC_TRNS,		KC_F7,		 KC_F8,		KC_F9,		 KC_F10},	
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [GAME] = {
   {KC_Q,	    KC_W,		   KC_E,		  KC_R,		    KC_T,	    KC_TRNS,	    KC_Y,		KC_U,		 KC_I,		 KC_O,		   KC_P},	
   {KC_A,	    KC_S,		   KC_D,		  KC_F,		    KC_G,	    KC_TRNS,	    KC_H,		KC_J,		 KC_K,		 KC_L,		   KC_SCLN},	
-  {KC_Z,	    KC_X,		   KC_C,		  KC_V,		    KC_B,	    KC_LCTL,	    KC_N,		KC_M,		 KC_COMM,	 KC_DOT,	   KC_SLSH},	
-  {KC_TRNS,	    KC_TRNS,		   KC_TRNS,		  KC_TAB,	    KC_SPC,	    KC_LALT,	    KC_SPC,		KC_QUOT,	 KC_TRNS,	 KC_ESC,	   KC_ENT}
+  {KC_Z,	    KC_X,		   KC_C,		  KC_V,		    KC_B,	    KC_TRNS,	    KC_N,		KC_M,		 KC_COMM,	 KC_DOT,	   KC_SLSH},	
+  {KC_TRNS,	    KC_TRNS,		   KC_TRNS,		  KC_TAB,	    KC_SPC,	    KC_TRNS,	    KC_SPC,		KC_QUOT,	 KC_TRNS,	 KC_TRNS,	   KC_ENT}
 }
 };
 
