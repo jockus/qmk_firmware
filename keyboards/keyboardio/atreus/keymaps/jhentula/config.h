06ad427218c31b597da9e6cd04791500cf44ad11
Joakim Hentula <joakim.hentula@gmail.com>
2020-06-22 17:35:10 +0100

    Dual key timeout tweak

--- keyboards/keyboardio/atreus/keymaps/jhentula/config.h
+++ keyboards/keyboardio/atreus/keymaps/jhentula/config.h
@@ -4,7 +4,7 @@
 #include "../../config.h"
 
 #define PREVENT_STUCK_MODIFIERS
-#define TAPPING_TERM 300
+#define TAPPING_TERM 150
 
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          5
